36399390e0c2b43c4836ee04ca9b47e6d9fab941
BIBLETUM <pasha.remizov@mail.ru>
2024-06-20 17:08:13 +0300

    Added CommunityCard

---
+++ app/src/main/java/ru/wb/meetings/domain/CommunityCardItem.kt
@@ -0,0 +1,10 @@
+package ru.wb.meetings.domain
+
+import ru.wb.meetings.R
+
+data class CommunityCardItem (
+    val id: Int,
+    val communityName: String = "Disigna",
+    val communitySubscribersCount: Int = 10000,
+    val communityImage: Int = R.drawable.community_image,
+)
--- app/src/main/java/ru/wb/meetings/presentation/components/CommunityCard.kt
+++ app/src/main/java/ru/wb/meetings/presentation/components/CommunityCard.kt
@@ -1,2 +1,63 @@
 package ru.wb.meetings.presentation.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.wb.meetings.domain.CommunityCardItem
+import ru.wb.meetings.presentation.theme.BodyText1
+import ru.wb.meetings.presentation.theme.Metadata1
+import ru.wb.meetings.presentation.theme.NeutralColorActive
+import ru.wb.meetings.presentation.theme.NeutralColorDisabled
+import ru.wb.meetings.presentation.theme.NeutralColorLine
+import java.util.Locale
+
+@Preview
+@Composable
+fun CommunityCard(
+    modifier: Modifier = Modifier,
+    communityCardItem: CommunityCardItem = CommunityCardItem(id = 0),
+) {
+    Column(modifier = modifier.background(NeutralColorLine)) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            MeetingAvatar(imageResId = communityCardItem.communityImage)
+
+            Column(modifier = Modifier.weight(1f)) {
+                Box(modifier = Modifier.height(24.dp), contentAlignment = Alignment.Center) {
+                    Text(
+                        text = communityCardItem.communityName,
+                        style = MaterialTheme.typography.BodyText1,
+                        color = NeutralColorActive,
+                    )
+                }
+
+                Box(modifier = Modifier.height(20.dp), contentAlignment = Alignment.Center) {
+                    Text(
+                        text = String.format(
+                            Locale.US,
+                            "%,d человек",
+                            communityCardItem.communitySubscribersCount
+                        ),
+                        style = MaterialTheme.typography.Metadata1,
+                        color = NeutralColorDisabled,
+                        modifier = Modifier.height(20.dp)
+                    )
+                }
+            }
+        }
+        HorizontalDivider(modifier = Modifier.padding(top = 12.dp))
+    }
+}
